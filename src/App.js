06d412d517a67cafb1df2cf7fd0bd4a9e3bb2251
Sam Hart <s.w.hart22@gmail.com>
2018-08-02 20:55:16 -0400

    pushing dev version to github

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import data from './Data.js';
 import face from './face_copy.png';
 import * as Scroll from 'react-scroll';
+import ResumePDF from './resume.pdf';
+
+const d3 = Object.assign({}, require('d3-time-format'));
 
 let Link = Scroll.Link;
+
+
 
 class Intro extends React.Component{
 	constructor(props){
@@ -13,8 +18,8 @@ class Intro extends React.Component{
 			type: 'visual'
 		};
 	}
-	/*componentDidMount(){
-		const types = ['visual','data','investigative'];
+	componentDidMount(){
+		const types = ['visual','data','digital'];
 		let init = 1;
 		
 		setInterval(()=>{
@@ -22,11 +27,11 @@ class Intro extends React.Component{
 				type: types[init++ % types.length]
 			})
 		}, 3000)
-	}*/
+	}
 
 	render(){
 		return(
-			<div id="intro-cont">
+			
 				<div id="intro">
 					<div id="intro-flex-left">
 						<img src={face}></img>
@@ -34,29 +39,65 @@ class Intro extends React.Component{
 					<div id="intro-flex-right">
 						<p id="intro-name">Sam Hart</p>
 						<p><span id="journo">{this.state.type}</span> journalist</p>
-						<Link to="projects" smooth={true}>Projects</Link>
-						<Link to="writing" smooth={true}>Writing</Link>
+						
 					</div>
 				</div>
-			</div>
+			
 
 			)
 	}
 }
-//list item component
+//create about me section
+class About extends React.Component{
+	render() {
+		//<Link to="writing" smooth={true}><span className="about-nav" id="about-writing">Writing/Reporting</span></Link>
+		//<Link to="resume" smooth={true}><span className="about-nav" id="about-resume">Resume</span></Link>
+		return (
+			<div id="about">
+				<div>
+				I'm a journalist who loves to code and design. I currently do so as a multimedia editor for NBC's Owned Television Stations. Check out my: <br /><br />
+					<Link to="projects" smooth={true}><span className="about-nav" id="about-gfx">Projects</span></Link>
+					<a href={ResumePDF} target="_blank"><span className="about-nav" id="about-resume">Resume</span></a>
+				</div>
+			</div>
+			)
+	}
+}
+
+//list item component, used in Projects and Writing
 class Item extends React.Component{
 	render(){
+		const skillslist = this.props.skills.map((d, i) => {
+			
+			<Skill
+				key={i}
+				skill={d}
+			/>
+		});
+		
 		return (
-			<div className="item-project">
-				<p className="item-date"><span></span>{this.props.date}</p>
-				<p><a href={this.props.url} target="_blank">{this.props.name}</a></p>
-				
-				<img src={this.props.image} />
-			</div>
+			<a href={this.props.url} target="_blank">
+				<div className="item-project">
+					<p className="item-date"><span>{this.props.date}</span></p>
+					<img className="item-image" src={process.env.PUBLIC_URL + 'assets/' + this.props.image} />
+					<p className="item-title">{this.props.name}</p>
+					<p className="skills">{skillslist}</p>
+				</div>
+			</a>
 			);
 	}
 }
-//generate list fromd data
+class Skill extends React.Component{
+	render(){
+		console.log(this.props.skill);
+		return (
+			<span>
+				{this.props.skill}
+			</span>
+		);
+	}
+}
+//generate list from data
 class Projects extends React.Component {
 	constructor(props){
 		super(props);
@@ -67,10 +108,11 @@ class Projects extends React.Component {
 	}
 	componentDidMount(){	
 		data.then(data => {
+			// console.log(data);
 			this.setState({
 				projects: data
 			});
-		})
+		});
 	}
 	render(){
 
@@ -81,9 +123,9 @@ class Projects extends React.Component {
 				date={e['date']}
 				image={e['image']}
 				url={e['url']}
+				skills={e['skills'].split(', ')}
 			/>
 		);
-		
 		
 		return (<div id="projects">{thelist}</div>);
 	};
@@ -96,14 +138,19 @@ class Writing extends React.Component{
 		)
 	}
 }
+
 //put it all together
 class App extends React.Component {
 	render(){
 		return (
 			<div id="container">
-				<Intro />
-				<Projects name="projects"/>
-				<Writing name="writing"/>
+				<div id="intro-cont">
+					<Intro />
+					<About />
+				</div>
+					<Projects name="projects"/>
+					<Writing name="writing"/>
+					
 			</div>)
 	}
 }
--- src/Data.js
+++ src/Data.js
@@ -1,19 +1,26 @@
 const sheetsy = require('sheetsy');
 const { urlToKey, getWorkbook, getSheet } = sheetsy;
+const d3 = Object.assign({}, require('d3-time-format'));
+
 
 const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1f0Shxx6gJFh8rkNHuyBlRZdH6LehONXZ055OTMrzOg8/edit#gid=0';
 
 const key = urlToKey(SPREADSHEET_URL);
 
+let timeParse = d3.timeParse('%m/%e/%y');
+//let timeFormat = d3.timeFormat('%B %Y');
+let timeFormat = d3.timeFormat('%m.%d.%y');
+
 const data = getSheet(key, 'od6').then(stuff => {
 	
 	let data = [];
 
-	function Project(date, desc, img, org, name, slug, type, url){
-		this.date = date;
+	function Project(date, desc, img, org, skills, name, slug, type, url){
+		this.date = timeFormat(timeParse(date));
 		this.description = desc;
 		this.image = img;
 		this.org = org;
+		this.skills = skills;
 		this['project-name'] = name;
 		this['project-slug'] = slug;
 		this.type = type;
@@ -26,6 +33,7 @@ const data = getSheet(key, 'od6').then(stuff => {
 			d['description'],
 			d['image'],
 			d['org'],
+			d['skills'],
 			d['project-name'],
 			d['project-slug'],
 			d['type'],
